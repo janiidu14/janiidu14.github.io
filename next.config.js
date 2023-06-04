339a05f03aa5f796a8438bf7990bbc44f5f0ac2d
Janidu Rathnayaka <86180073+janiidu14@users.noreply.github.com>
2023-06-04 20:52:30 +0530

    Commit Project

---
+++ next.config.js
@@ -0,0 +1,42 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+  });
+   
+  const withPWA = require("next-pwa");
+   
+  module.exports = withBundleAnalyzer(
+    withPWA({
+      webpack: true,
+      webpack: (config) => {
+        // Fixes npm packages that depend on `fs` module
+        config.resolve.fallback = { fs: false };
+        return config;
+      },
+      reactStrictMode: true,
+      images: {
+        domains: [
+          "cdn.buymeacoffee.com",
+          "res.cloudinary.com",
+          "imgur.com",
+          "i.imgur.com",
+          "cutt.ly",
+          "activity-graph.herokuapp.com",
+          "images.unsplash.com",
+        ],
+      },
+   
+      // Pwa Setting
+      pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+        publicExcludes: ["!resume.pdf"], 
+      },
+    })
+  );
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
+        sarina: ["Sarina", "cursive"],
+        barlow: ["Barlow", "sans-serif"],
+        mono: ["monospace"],
+      },
+      extend: {
+        colors: {
+          darkPrimary: "#181A1B",
+          darkSecondary: "#25282A",
+          darkWhite: "#f2f5fa",
+        },
+        listStyleType: {
+          square: "square",
+          roman: "upper-roman",
+        },
+        animation: {
+          wiggle: "wiggle 1s ease-in-out infinite",
+          "photo-spin": "photo-spin 2s 1 linear forwards",
+        },
+        keyframes: {
+          wiggle: {
+            "0%, 100%": { transform: "rotate(-3deg)" },
+            "50%": { transform: "rotate(3deg)" },
+          },
+          "photo-spin": {
+            "0%": { transform: "rotate(0deg)" },
+            "100%": { transform: "rotate(360deg)" },
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
