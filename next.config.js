322c61df19210b618bd8a69bed3583944b001f78
Janidu Rathnayaka <86180073+janiidu14@users.noreply.github.com>
2023-06-05 22:48:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,36 +1,51 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+    enabled: process.env.ANALYZE === "true",
+  });
+   
+  const withPWA = require("next-pwa");
+   
+  module.exports = withBundleAnalyzer(
+    withPWA({
+      webpack: true,
+      webpack: (config) => {
+        // Fixes npm packages that depend on `fs` module
+        config.resolve.fallback = { fs: false };
+        return config;
+      },
+      reactStrictMode: true,
+      images: {
+        domains: [
+          "cdn.buymeacoffee.com",
+          "res.cloudinary.com",
+          "imgur.com",
+          "i.imgur.com",
+          "cutt.ly",
+          "activity-graph.herokuapp.com",
+          "images.unsplash.com",
+        ],
+      },
+   
+      // Pwa Setting
+      pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+        publicExcludes: ["!resume.pdf"], 
+      },
 
-const withPWA = require("next-pwa");
-
-module.exports = withBundleAnalyzer(
-  withPWA({
-    webpack: (config) => {
-      // Fixes npm packages that depend on `fs` module
-      config.resolve.fallback = { fs: false };
-      return config;
+      // Export Path Map Configuration
+    exportPathMap: async function () {
+      // Define the export paths for your application
+      return {
+        "/": { page: "/" },
+        // Add more export paths as needed
+      };
     },
-    reactStrictMode: true,
-    images: {
-      domains: [
-        "cdn.buymeacoffee.com",
-        "res.cloudinary.com",
-        "imgur.com",
-        "i.imgur.com",
-        "cutt.ly",
-        "activity-graph.herokuapp.com",
-        "images.unsplash.com",
-      ],
-    },
-
-    // PWA Setting
-    pwa: {
-      dest: "public",
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === "development",
-      publicExcludes: ["!resume.pdf"],
-    },
-  })
-);
+    })
+  );
