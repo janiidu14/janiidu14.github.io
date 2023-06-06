fd3a2a78609990d9fbf60653e2648d08364f8591
Janidu Rathnayaka <86180073+janiidu14@users.noreply.github.com>
2023-06-06 10:28:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,42 +7,33 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-  });
+module.exports = withBundleAnalyzer(
+  withPWA({
+    webpack: (config) => {
+      // Fixes npm packages that depend on `fs` module
+      config.resolve.fallback = { fs: false };
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "cdn.buymeacoffee.com",
+        "res.cloudinary.com",
+        "imgur.com",
+        "i.imgur.com",
+        "cutt.ly",
+        "activity-graph.herokuapp.com",
+        "images.unsplash.com",
+      ],
+    },
    
-  const withPWA = require("next-pwa");
-   
-  module.exports = withBundleAnalyzer(
-    withPWA({
-      webpack: true,
-      webpack: (config) => {
-        // Fixes npm packages that depend on `fs` module
-        config.resolve.fallback = { fs: false };
-        return config;
-      },
-      reactStrictMode: true,
-      images: {
-        domains: [
-          "cdn.buymeacoffee.com",
-          "res.cloudinary.com",
-          "imgur.com",
-          "i.imgur.com",
-          "cutt.ly",
-          "activity-graph.herokuapp.com",
-          "images.unsplash.com",
-        ],
-      },
-   
-      // Pwa Setting
-      pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === "development",
-        publicExcludes: ["!resume.pdf"], 
-      },
-    })
-  );
+    // PWA Setting
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+      publicExcludes: ["!resume.pdf"], 
+    },
+  })
+);
